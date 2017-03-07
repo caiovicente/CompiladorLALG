44367fe23abbb29f0b53015f616644c7a98c9920
33236 <33236@CTJ-185582>
2017-03-07 10:04:03 -0400

    Add project files.

---
+++ Compilador/Token.cs
@@ -0,0 +1,22 @@
+﻿namespace Compilador
+{
+    public class Token
+    {
+        public string ident, tipo;
+        public Token proximo;
+
+        public Token()
+        {
+            ident = "";
+            tipo = "";
+            proximo = null;
+        }
+
+        public Token(string id, string tipo)
+        {
+            this.ident = id;
+            this.tipo = tipo;
+            this.proximo = null;
+        }
+    }
+}
