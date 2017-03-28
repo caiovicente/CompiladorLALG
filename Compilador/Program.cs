6d53be28307c2b6a30a206e7ca2378033893ad40
caiovicente <henriquecaiomt@hotmail.com>
2017-03-28 12:51:49 -0400

    Alterações no Program para imprimir separadamente ID e TIPO do token

--- Compilador/Program.cs
+++ Compilador/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Compilador
 {
@@ -24,6 +27,7 @@ namespace Compilador
             }
             codigo.Close();
             Console.ReadKey();
+            
         }
     }
 }
