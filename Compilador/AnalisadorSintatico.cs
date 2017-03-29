f94c12190715f81a10121adf2696c962603bba38
caiovicente <henriquecaiomt@hotmail.com>
2017-03-29 11:11:42 -0400

    Começando a escrever analise sintática

--- Compilador/AnalisadorSintatico.cs
+++ Compilador/AnalisadorSintatico.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace Compilador
 {
     public class AnalisadorSintatico
@@ -15,13 +16,41 @@ namespace Compilador
             token = new Token();
             this.analisadorLexico = analisadorLexico;
             if (programa())
-            {
                 return true;
-            }
             return false;
         }
         
-        public bool programa()
+        private bool programa()
+        {
+            token = analisadorLexico.retornaToken();
+            if (token.id == "program")
+                token = analisadorLexico.retornaToken();
+            else
+            {
+                Console.WriteLine("Falta identificador de inicio 'program'");
+                return false;
+            }
+
+
+            if (token.tipo == "Identificador")
+                token = analisadorLexico.retornaToken();
+            else
+            {
+                Console.WriteLine("Nome do programa faltando.");
+                return false;
+            }
+            if (corpo())
+            {
+                Console.WriteLine("Parte 1 passou");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool corpo()
         {
             return true;
         }
